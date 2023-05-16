92ec533732c0ff21dbccdf104bdc2f97d52c9027
franck <carnet.franck.paul@gmail.com>
2023-05-16 13:11:06 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -11,11 +11,12 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Anti flood'] = '';
-L10n::$locales['Service Temporarily Unavailable'] = '';
-L10n::$locales['The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later.'] = '';
-L10n::$locales['Delay:'] = '';
-L10n::$locales['Sets the delay in seconds beetween two comments from the same IP'] = '';
-L10n::$locales['Send error code:'] = '';
-L10n::$locales['Sets whether the filter should reply with a 503 error code.'] = '';
-L10n::$locales['Save'] = '';
+L10n::$locales['Anti flood'] = 'Anti flood';
+L10n::$locales['Service Temporarily Unavailable'] = 'Service temporairement indisponible';
+L10n::$locales['The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later.'] = 'Le serveur est temporairement incapable de traiter votre requête à cause d\'une maintenance ou d\'un problème de capacité.
+Essayez à nouveau dans un moment.';
+L10n::$locales['Delay:'] = 'Délai :';
+L10n::$locales['Sets the delay in seconds beetween two comments from the same IP'] = 'Indiquez le délai en secondes entre deux commentaires pour une même adresse IP';
+L10n::$locales['Send error code:'] = 'Envoyer le code d\'erreur :';
+L10n::$locales['Sets whether the filter should reply with a 503 error code.'] = 'Indique si le filtre doit répondre avec un code d\'erreur 503';
+L10n::$locales['Save'] = 'Enregistrer';
