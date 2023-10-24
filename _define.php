fa29d44a8ee280c48aefbaad08fa43ac9667150c
franck <carnet.franck.paul@gmail.com>
2023-10-24 19:37:42 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'antiflood',
     'Anti flood spam filter',
     'dcTeam',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
